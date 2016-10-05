d3a9928fb6c1868a25f4cf60443cee362aa9b6d2
Joe Egan <joeegan84@gmail.com>
2016-10-05 23:07:12 +0100

    reninstate service-worker

--- src/index.js
+++ src/index.js
@@ -4,5 +4,11 @@ import App from './app';
 import styles from './styles.css';
 import stopwatch from './stopwatch.css';
 import favicon from './favicon.ico';
+import sw from './service-worker';
+
+const register = require('serviceworker!./service-worker.js');
+register({ scope: '/' })
+  .then(() => console.log('Service worker caching resources'))
+  .catch(err => console.log('Service worker failed to load', err));
 
 render(<App />, document.querySelector('#root'));
