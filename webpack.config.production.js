512b19c2ede698a681c1709d9333a4b28a8517fd
Joe Egan <joeegan84@gmail.com>
2016-10-05 20:57:21 +0100

    slap the css into the js bundle

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -36,6 +36,10 @@ module.exports = {
         loaders: ['babel'],
         include: path.join(__dirname, 'src'),
       },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader',
+      },
     ],
   },
 };
